16ee751e12fd7e46d6cb5cc793a60ddff6b327c7
Hendrik Schmitz <hendrik.schmitz@aixigo.com>
2021-04-30 11:53:09 +0200

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import { initFacebookSdk } from "./helper/facebook-sdk-helper";
 import App from "./App.vue";
 import router from "./router";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
 initFacebookSdk().then(() => {
   new Vue({
     router,
+    vuetify,
     render: (h) => h(App),
   }).$mount("#app");
 });
